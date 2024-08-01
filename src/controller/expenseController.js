606a7f30afe83083ab152f55c80fcbbdf50d74c4
Th1234wee <theara6574@gmail.com>
2024-08-01 12:03:21 +0700

    Finished Add Expense All

--- src/controller/expenseController.js
+++ src/controller/expenseController.js
@@ -7,15 +7,24 @@ export const addNewExpenseController = (request,response) => {
         if(error) return response.status(500).json({
             message : "Something Went Wrong"
         })
-        console.log(row);
+        // console.log(row[0].id);
+        if(!title && !type && !category && !description && !expense_amount){
+            return response.status(400).json({
+                message : "Bad Request"
+            })
+        }
+    
+        const sql_insert = `INSERT INTO expense (title,type,category,description,expense_amount,user_id)
+                                        VALUES (?,?,?,?,?,?)`;
+        const insertValue = [title , type , category , description , expense_amount , row[0].id]
+        pool.query(sql_insert , insertValue , (error,result) => {
+            if(error) return response.status(500).json({
+                message : "Add Failed",
+            })
+            response.status(200).json({
+                message : "Add Success",
+                result
+            })
+        })
     })
-
-    // if(!title && !type && !category && !description && !expense_amount){
-    //     return response.status(401).json({
-    //         message : "Bad Request"
-    //     })
-    // }
-
-    // const sql_insert = `INSERT INTO expense (title,type,category,description,expense_amount,user_id)
-    //                                 VALUES (?,?,?,?,?,?)`;
 }
